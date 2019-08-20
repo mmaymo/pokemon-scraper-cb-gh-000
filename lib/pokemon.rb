19869e1e74c8daa58bc84e49ce8914e5e352586a
carmenmaymo <mayen22@gmail.com>
2019-08-20 12:58:34 +0200

    created table and pokemon class. All tests pass

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  def initialize( id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+  end
+  def self.find(id_to_find, db)
+
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id_to_find).flatten
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type:pokemon[2], db: db)
+  end
 end
